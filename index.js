c5998896072b32328a810e3f37968f5b4df65dfa
trexdip <trexdip@hotmail.com>
2017-11-30 10:35:01 -0800

    seconds

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-var counter;
+var i=0;
+var counter=true;
 var x=5;
 var y=10;
-while(x+y<57)
+var array=new Array()
+
+function conditional()
+	{
+		if(x+y>57)
+			{
+				counter=false;
+			}
+	}
+function conditional1()
+	{
+		if ((x+y)==19)
+		{
+			console.log("Number of Iterations completed:"+i)
+		}
+	}
+
+
+while(counter)
 {
-	console.log("Simple example of Do While Loop")
-	x=x+1
-	y=y+1 
-	counter=false;
+	i=i+1;
+	console.log("Simple example of While Loop");
+	x=x+1;
+	y=y+1;
+	conditional1();
+	conditional();
 }
